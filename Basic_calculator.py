3560a827889a7acb39a007e55f0afc1aa9efa211
Manu Shukla <manushukla64@gmail.com>
2025-05-06 16:01:36 +0530

    raise_exceptions

--- Basic_calculator.py
+++ Basic_calculator.py
@@ -1,32 +1,46 @@
+from csv import excel_tab
+
+
 def calculator():
     while True:
-        print("\nSimple Calculator")
-        number1 = float(input("Enter first number: "))
-        assert number1 >= 0, "Only Positive Number!"
+        try:
+            print("\nSimple Calculator")
+            number1 = float(input("Enter first number: "))
+            assert number1 >= 0, "Only Positive Number!"
 
-        operation = input("Enter the operation you would like to perform (+, -, *, /) or 'q' to quit: ")
-        if operation == 'q':
-            print("Goodbye!")
-            break
+            operation = input("Enter the operation you would like to perform (+, -, *, /) or 'q' to quit: ")
+            if operation == 'q':
+                print("Goodbye!")
+                break
 
-        number2 = float(input("Enter second number: "))
-        assert number2 >= 0, "Only Positive Number!"
+            number2 = float(input("Enter second number: "))
+            assert number2 >= 0, "Only Positive Number!"
 
-        if operation == '+':
-            result = number1 + number2
-        elif operation == '-':
-            result = number1 - number2
-        elif operation == '*':
-            result = number1 * number2
-        elif operation == '/':
-            assert number2 != 0, "Can't divide by zero"
-            result = number1 / number2
-        else:
-            print("Invalid operation!")
-            continue  # skip to next loop iteration
+            if operation == '+':
+                result = number1 + number2
+            elif operation == '-':
+                result = number1 - number2
+            elif operation == '*':
+                result = number1 * number2
+            elif operation == '/':
+                if number2 == 0:
+                    raise ZeroDivisionError("Cannot divide by zero.")
+                result = number1 / number2
+            else:
+                print("Invalid operation!")
+                continue  # skip to next loop iteration
+            print("Result:", result)
 
-        print("Result:", result)
+        except ValueError:
+               print("❌ Please enter valid numbers.")
+        except AssertionError as ae:
+            print("❌", ae)
+        except ZeroDivisionError as zde:
+            print("❌", zde)
+        except Exception as e:
+            print("Unexpected error:", e)
 calculator()
+
 # earlier in my code there were issues related to how i was calling the function --
 """
 def calculator():
